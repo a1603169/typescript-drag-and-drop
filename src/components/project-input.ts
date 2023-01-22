cbe8cd49f7ec31b404eac419a710339ef14bc295
seunghunbang <upi21x@gmail.com>
2023-01-22 08:59:36 +0200

    learning namespace webpack config

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,89 +1,88 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../util/validation.ts"/>
-/// <reference path="../state/project.ts" />
-namespace App {
-  // ProjectInput Class
+import { Component } from "./base-component";
+import { Validatable, validate } from "../util/validation";
+import { autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
 
-  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
-    titleInputElement: HTMLInputElement;
-    descriptionInputElement: HTMLTextAreaElement;
-    peopleInputElement: HTMLInputElement;
+// ProjectInput Class
 
-    constructor() {
-      super("project-input", "app", true, "user-input");
-      this.titleInputElement = this.element.querySelector(
-        "#title"
-      ) as HTMLInputElement;
-      this.descriptionInputElement = this.element.querySelector(
-        "#description"
-      ) as HTMLTextAreaElement;
-      this.peopleInputElement = this.element.querySelector(
-        "#people"
-      ) as HTMLInputElement;
-      this.configure();
-      this.renderContent();
-    }
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLTextAreaElement;
+  peopleInputElement: HTMLInputElement;
 
-    private gatherUserInput(): [string, string, number] | void {
-      const enteredTitle = this.titleInputElement.value;
-      const enteredDescription = this.descriptionInputElement.value;
-      const enteredPeople = this.peopleInputElement.value;
-
-      const titleValidatable: Validatable = {
-        value: enteredTitle,
-        required: true,
-      };
-      const descriptionValidatable: Validatable = {
-        value: enteredDescription,
-        required: true,
-        minLength: 5,
-      };
-      const peopleValidatable: Validatable = {
-        value: +enteredPeople,
-        required: true,
-        min: 1,
-        max: 5,
-      };
-
-      if (
-        !validate(titleValidatable) ||
-        !validate(descriptionValidatable) ||
-        !validate(peopleValidatable)
-      ) {
-        alert("Invalid input, please try again!");
-        console.log(
-          validate(titleValidatable),
-          validate(descriptionValidatable),
-          validate(peopleValidatable)
-        );
-        return;
-      } else {
-        return [enteredTitle, enteredDescription, +enteredPeople];
-      }
-    }
-
-    @autobind
-    private submitHandler(event: Event) {
-      event.preventDefault();
-      const userInput = this.gatherUserInput();
-      if (Array.isArray(userInput)) {
-        const [title, desc, people] = userInput;
-        projectState.addProject(title, desc, people);
-        this.clearInputs();
-      }
-    }
-
-    private clearInputs() {
-      this.titleInputElement.value = "";
-      this.descriptionInputElement.value = "";
-      this.peopleInputElement.value = "";
-    }
-
-    configure() {
-      this.element.addEventListener("submit", this.submitHandler);
-    }
-
-    renderContent(): void {}
+  constructor() {
+    super("project-input", "app", true, "user-input");
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLTextAreaElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+    this.configure();
+    this.renderContent();
   }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("Invalid input, please try again!");
+      console.log(
+        validate(titleValidatable),
+        validate(descriptionValidatable),
+        validate(peopleValidatable)
+      );
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      projectState.addProject(title, desc, people);
+      this.clearInputs();
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  renderContent(): void {}
 }
--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,49 +1,45 @@
-namespace App {
-  // Validation
+export interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
 
-  export interface Validatable {
-    value: string | number;
-    required?: boolean;
-    minLength?: number;
-    maxLength?: number;
-    min?: number;
-    max?: number;
+export function validate(valirdateableInput: Validatable) {
+  let isValid = true;
+  if (valirdateableInput.required) {
+    isValid =
+      isValid && valirdateableInput.value.toString().trim().length !== 0;
   }
-
-  export function validate(valirdateableInput: Validatable) {
-    let isValid = true;
-    if (valirdateableInput.required) {
-      isValid =
-        isValid && valirdateableInput.value.toString().trim().length !== 0;
-    }
-    if (
-      valirdateableInput.minLength != null &&
-      typeof valirdateableInput.value === "string"
-    ) {
-      isValid =
-        isValid &&
-        valirdateableInput.value.length >= valirdateableInput.minLength;
-    }
-    if (
-      valirdateableInput.maxLength != null &&
-      typeof valirdateableInput.value === "string"
-    ) {
-      isValid =
-        isValid &&
-        valirdateableInput.value.length <= valirdateableInput.maxLength;
-    }
-    if (
-      valirdateableInput.min != null &&
-      typeof valirdateableInput.value === "number"
-    ) {
-      isValid = isValid && valirdateableInput.value >= valirdateableInput.min;
-    }
-    if (
-      valirdateableInput.max != null &&
-      typeof valirdateableInput.value === "number"
-    ) {
-      isValid = isValid && valirdateableInput.value <= valirdateableInput.max;
-    }
-    return isValid;
+  if (
+    valirdateableInput.minLength != null &&
+    typeof valirdateableInput.value === "string"
+  ) {
+    isValid =
+      isValid &&
+      valirdateableInput.value.length >= valirdateableInput.minLength;
   }
+  if (
+    valirdateableInput.maxLength != null &&
+    typeof valirdateableInput.value === "string"
+  ) {
+    isValid =
+      isValid &&
+      valirdateableInput.value.length <= valirdateableInput.maxLength;
+  }
+  if (
+    valirdateableInput.min != null &&
+    typeof valirdateableInput.value === "number"
+  ) {
+    isValid = isValid && valirdateableInput.value >= valirdateableInput.min;
+  }
+  if (
+    valirdateableInput.max != null &&
+    typeof valirdateableInput.value === "number"
+  ) {
+    isValid = isValid && valirdateableInput.value <= valirdateableInput.max;
+  }
+  return isValid;
 }
